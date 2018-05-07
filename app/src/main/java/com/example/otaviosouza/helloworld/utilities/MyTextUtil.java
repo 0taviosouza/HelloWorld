0c7fc9e271688d2184c9d1109ebe8bb36ac83fdb
Otavio Souza <otavio.workmail@gmail.com>
2018-05-07 12:30:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/otaviosouza/helloworld/utilities/MyTextUtil.java
@@ -0,0 +1,16 @@
+package com.example.otaviosouza.helloworld.utilities;
+
+public class MyTextUtil {
+    private String name;
+
+    public MyTextUtil() {
+    }
+
+    public MyTextUtil(String name) {
+        this.name = name;
+    }
+
+
+    public void doSomethingElse() {
+    }
+}
